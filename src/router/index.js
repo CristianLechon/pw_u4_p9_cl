9995df859a97a12f25995799cd0fa31934b63751
CristianLechon <calechon@uce.edu.ec>
2026-02-05 18:44:03 -0500

    Taller 36

--- src/router/index.js
+++ src/router/index.js
@@ -6,48 +6,95 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import('../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      requiereAutorizacion: false,
+      esPublica: false
+    }
   },
   {
     path: '/guardar',
     name: 'guardar',
-    component: () => import('../views/GuardarView.vue')
+    component: () => import('../views/GuardarView.vue'),
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/listarTodos',
     name: 'listarTodos',
-    component: () => import('../views/ListarTodosView.vue')
+    component: () => import('../views/ListarTodosView.vue'),
+    meta: {
+      requiereAutorizacion: false,
+      esPublica: false
+    }
   },
   {
     path: '/consultarId',
     name: 'consultarId',
-    component: () => import('../views/ConsultarIdView.vue')
+    component: () => import('../views/ConsultarIdView.vue'),
+    meta: {
+      requiereAutorizacion: false,
+      esPublica: false
+    }
   },
   {
     path: '/actualizar',
     name: 'actualizar',
-    component: () => import('../views/ActualizarView.vue')
+    component: () => import('../views/ActualizarView.vue'),
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/actualizarParcial',
     name: 'actualizarParcial',
-    component: () => import('../views/ActualizarParcialView.vue')
+    component: () => import('../views/ActualizarParcialView.vue'),
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
   {
     path: '/eliminar',
     name: 'eliminar',
-    component: () => import('../views/EliminarView.vue')
+    component: () => import('../views/EliminarView.vue'),
+    meta: {
+      requiereAutorizacion: true,
+      esPublica: false
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+/* Configuracion del Guardian */
+//to es a la pagina que quiero acceder
+//from desde que pagina vengo 
+//next es una variable que me permite decir si le permito ir a la pagina o lo redireccion
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiereAutorizacion) {
+    /* le envio a una pagina de login */
+    console.log("Redirige a Login");
+  } else {
+    /* le dejo pasar sin validacion */
+    console.log("Pase Libre");
+    next(); // siga
+  }
 })
 
 export default router
